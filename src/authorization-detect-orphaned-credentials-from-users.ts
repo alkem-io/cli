0fbab06ddeb978afc9e93c158b28793ed7efbea4
Neil Smyth <neil@thesmyths.eu>
2021-09-29 11:04:18 +0200

    extended check for invalid authorization credentials

--- src/authorization-detect-orphaned-credentials-from-users.ts
+++ src/authorization-detect-orphaned-credentials-from-users.ts
@@ -16,6 +16,8 @@ const main = async () => {
 
   // get all the ecoverses + all the challenges
   const challengesMap = new Map();
+  const userGroupsMap = new Map();
+  const opportunitiesMap = new Map();
   const ecoversesMap = new Map();
   const ecoverses = (await alClient.client.ecoverses()).data?.ecoverses;
   if (ecoverses) {
@@ -25,6 +27,18 @@ const main = async () => {
       if (challenges) {
         for (const challenge of challenges) {
           challengesMap.set(challenge.id, challenge);
+        }
+      }
+      const userGroups = await alClient.groups(ecoverse.nameID);
+      if (userGroups) {
+        for (const userGroup of userGroups) {
+          userGroupsMap.set(userGroup.id, userGroup);
+        }
+      }
+      const opportunities = await alClient.opportunities(ecoverse.nameID);
+      if (opportunities) {
+        for (const opportunity of opportunities) {
+          opportunitiesMap.set(opportunity.id, opportunity);
         }
       }
     }
@@ -66,6 +80,30 @@ const main = async () => {
             if (!challengesMap.has(credential.resourceID)) {
               logger.warn(
                 `[Challenge] Identified ChallengeAdmin credential for not existing challenge: ${credential.resourceID}`
+              );
+            }
+          } else if (
+            credential.type === AuthorizationCredential.OpportunityMember
+          ) {
+            if (!opportunitiesMap.has(credential.resourceID)) {
+              logger.warn(
+                `[Opportunity] Identified member credential for not existing Opportunity: ${credential.resourceID}`
+              );
+            }
+          } else if (
+            credential.type === AuthorizationCredential.OpportunityAdmin
+          ) {
+            if (!opportunitiesMap.has(credential.resourceID)) {
+              logger.warn(
+                `[Opportunity] Identified admin credential for not existing Opportunity: ${credential.resourceID}`
+              );
+            }
+          } else if (
+            credential.type === AuthorizationCredential.UserGroupMember
+          ) {
+            if (!userGroupsMap.has(credential.resourceID)) {
+              logger.warn(
+                `[UserGroup] Identified group member credential for not existing userGroup: ${credential.resourceID}`
               );
             }
           }
