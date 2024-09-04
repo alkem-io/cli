fb11531e71fbd71eab7921133ae4bbd59bee9b79
Svetoslav <svetoslav@alkem.io>
2024-09-04 17:42:11 +0300

    fixes to existing scripts

--- src/prune-orphan-data/utils.ts
+++ src/prune-orphan-data/utils.ts
@@ -3,11 +3,22 @@ import { Relation } from './types/node';
 
 const ColumnsWithAllowedNullValues = ['createdBy'];
 export const createIsNullCheck = (str: string): string => {
-  if (str.endsWith('.id')) return '';
-  if (ColumnsWithAllowedNullValues.includes(str)) return '';
+  if (str.endsWith('.id')) {
+    return '';
+  }
+  if (ColumnsWithAllowedNullValues.includes(str)) {
+    return '';
+  }
   return ` OR ${str} IS NULL`;
 };
 
+export const createNotExistsCheck = (table: string, fk: Relation): string => {
+  return `NOT EXISTS (SELECT 1 FROM ${fk.node.name} WHERE ${fk.node.name}.${fk.refColumnName} = ${table}.${fk.refChildColumnName})`;
+};
+// does not take into account NULLS in the parent
+// NOT IN returns 0 records when compared against an unknown value (null)
+//  3 <> 1 and 3 <> 2 and 3 <> null
+// which evaluates to: TRUE and TRUE and UNKNOWN, which evaluates to: UNKNOWN
 export const createNotInCheck = (table: string, fk: Relation): string => {
   return `${table}.${fk.refChildColumnName} NOT IN (SELECT ${fk.refColumnName} FROM ${fk.node.name})`;
 };
@@ -19,7 +30,7 @@ export const addParentRelationsChecks = (
 ) => {
   for (const fk of parentRelations) {
     parentRelationsChecks.push(
-      `(${createNotInCheck(table.name, fk)} ${createIsNullCheck(
+      `(${createNotExistsCheck(table.name, fk)}${createIsNullCheck(
         `${table.name}.${fk.refChildColumnName}`
       )})`
     );
